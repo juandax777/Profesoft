c4ee38315c351d029f6c6dae23a87ea630bbabbf
Juan Name <juan@example.com>
2024-09-19 19:23:29 -0500

    (nueva): configuracion del proyecto con springboot

---
+++ src/main/java/co/ucentral/myrecicled/MyrecicledApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.myrecicled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrecicledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrecicledApplication.class, args);
+	}
+
+}
